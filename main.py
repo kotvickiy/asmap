4a9a48ad57bc5ba120898c070bcb625274650c87
kotvickiy <vladkotvickiy@gmail.com>
2023-10-25 20:03:20 +0500

    com

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hello!")
+from questions import *
+
+
+
+
+
+def fun_begin():
+    for q in quest:
+        print(q[0])
+        answer = input("? ")
+        if answer == q[1]:
+            print("ok")
+        else:
+            print("no")
+
+
+def main():
+    start = input("Начать?(д/н): ")
+    if start == "д" or start == "y" or start == "да" or start == "yes":
+        fun_begin()
+    else:
+        print("До свидания")
+
+
+if __name__ == '__main__':
+    main()
